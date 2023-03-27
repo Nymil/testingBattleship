b04b9c8f44d1e7b995030b7f09d3f77eaa277770
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-03-27 20:13:14 +0200

    changed board generation function to allow for choosing container to generate in

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,17 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    generateBoard(10, 10);
+    const $boardContainer = document.querySelector('.board-container');
+    generateBoard($boardContainer, 10, 10);
 }
 
-function generateBoard(cols, rows) {
-    const $boardTable = document.querySelector('.board-container')
+function generateBoard($container, cols, rows) {
     for (let row = 0; row < rows; row++) {
         let rowInsertion = "<tr>";
         for (let col = 0; col < cols; col++) {
             rowInsertion += `<td><div class="cell" data-col="${col}" data-row="${row}"></div></td>`;
         }
         rowInsertion += "</tr>";
-        $boardTable.insertAdjacentHTML('beforeend', rowInsertion);
+        $container.insertAdjacentHTML('beforeend', rowInsertion);
     }
 }
