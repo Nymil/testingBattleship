3c069a12445ecdbe37ea598d9130ab016f5db3da
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-03-27 19:50:34 +0200

    added board generator function that injects HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,17 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log("connected");
+    generateBoard(10, 10);
+}
+
+function generateBoard(cols, rows) {
+    const $boardTable = document.querySelector('.board-container')
+    for (let row = 0; row < rows; row++) {
+        let rowInsertion = "<tr>";
+        for (let col = 0; col < cols; col++) {
+            rowInsertion += `<td><div class="cell" data-col="${col}" data-row="${row}"></div></td>`;
+        }
+        rowInsertion += "</tr>";
+        $boardTable.insertAdjacentHTML('beforeend', rowInsertion);
+    }
 }
